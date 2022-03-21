44e5897943828fb8d2c85f0805dd32f02c16d290
Gorfs <archiebeales@gmail.com>
2022-03-21 16:13:00 +0100

    added some html for the form unstable

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def index():
 def index2():
   return render_template("index.html")
 
+#this is going to be the section about sql manipulation and how to put it into a website
+#todo make the input form for the html file, 
+#make the back end that will recieve the variables
+#make the sql database for storing the data
+#make html file show what is in the database
 
 
 
