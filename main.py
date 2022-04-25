c5927b04b592e164a61194f2c4b6202726ed4548
Gorfs <archiebeales@gmail.com>
2022-04-25 17:14:16 +0200

    added the leaderboard

--- main.py
+++ main.py
@@ -2,8 +2,37 @@ from flask import Flask, render_template, request, redirect
 from SQL import *
 import os # for path of the database
 
-leaderBoard = None
 
+
+class Score:
+  """
+  un object python utiliser pour stocker les informations de chaque score
+  """
+  def __init__(self, name : str, time : str) -> None:
+    self.playerName = name
+    self.score = time
+
+
+class LeaderBoard:
+  '''
+  une classe qui est utiliser pour stocker tous les informations concernant les score du demineur
+  '''
+  def __init__(self,DBscores : dict) -> None:
+    self.scores = []
+    print("DB output is " , DBscores)
+    for item in DBscores:
+      self.scores.append([item, DBscores[item]])
+ 
+
+
+  def get_scores(self) -> list:
+    """
+    returns the scores that were stored in the database in the form of a list of dictionary objects
+    """
+    return self.scores
+
+
+  
 
 
 app = Flask(__name__)
@@ -16,19 +45,33 @@ def index():
     print(retrieve_all_from_db(connection))
     return redirect(request.url)
   else:
-    return render_template("index.html")
+    print("leaderboard has been asked")
+    leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
+    print("the scores are {}".format(leaderboard.get_scores()))
+    return render_template("index.html", scores = leaderboard.get_scores())
 
 @app.route("/index.html", methods = ['POST', 'GET'])
 def index2():
   if request.method == 'POST':
-    print("AHA YOU POST REQUEST SCUM, PREPARE TO DIE")
+    print("The request was a post and thus was the reply with the form")
     insert_into_db(connection , request.form)
     return redirect(request.url)
   else:
-    return render_template("index.html")
+    print("leaderboard has been asked")
+    leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
+    print("the scores are {}".format(leaderboard.get_scores()))
+    return render_template("index.html", scores = leaderboard.get_scores())
+
+@app.route("/leaderboard.html", methods = ['GET'])
+def leaderboard()-> None:
+  print("leaderboard has been asked")
+  leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
+  print("the scores are {}".format(leaderboard.get_scores()))
+  return render_template("index.html", scores = leaderboard.get_scores())
+
 
 #this is going to be the section about sql manipulation and how to put it into a website
-#todo make the input form for the html file, 
+#todo make the input form for ,the html file, 
 #make the back end that will recieve the variables
 #make the sql database for storing the data
 #make html file show what is in the database
