0725624a43b02085c20fe3a1553814e72b22d72e
Gorfgorf23 <archiebeales@gmail.com>
2022-04-26 21:28:19 +0200

    updated the readme

--- READ_ME/README.md
+++ READ_ME/README.md
@@ -12,3 +12,48 @@ TODO: -Faire le structure de la page
 -trouver comment faire une page avec les information qui vient de python(templates?)
 -convertir les log de google drive vers ce reademe.md\
 -recontinuer a developper la fonction explode tiles pour montrere les nombres
+
+**Lundi 28/03** 
+
+*Objectif:*
+
+Faire lien form avec BDD avec programme python?
+
+Interface création profile + BDD pour profilé et lien avec form.réalité:	
+
+-réussite de test web server sur un pc hors que ceux du lycée	
+
+- réussite de mettre en place le form html pour les scores	
+- - réussite de recevoire ces score dans le fichier python	
+  - - reussite de commit la base de données et création de la premiere tables	
+    -  echec de commencer le bot selenium -création du fichier python pour interaction avec la BDD
+
+**Lundi 04/04**
+
+*Objectif:*	
+
+-cree le leaderboard	
+
+-modifier la fonction explode tiles pour faire les nombre (redesign)	
+
+-remplir le fichier python pour la base de donnée	
+
+-rajouter la possibilter de flag les tuiles
+
+*realiter:*	
+
+-quentin a apris ce que c’est une XPATH,	
+
+-quentin a fais un bot qui ouvre firefox, et CLICK	
+
+-quenting a appris ce que c’est une document xml
+
+**lundi 25/04**
+
+-mis en place de la page leaderboard sur le demineur
+
+-mis en place du code en python pour prendre les information de la base de donner et de le render sur la page web
+
+-continuation du travaille sur la fonction explode_tiles
+
+-le bot demineur peut maintenant choisir quelle cube il veut clicker dessus
--- main.py
+++ main.py
@@ -1,101 +1,100 @@
 from flask import Flask, render_template, request, redirect
 from SQL import *
-import os # for path of the database
-
+import os  # for path of the database
 
 
 class Score:
-  """
+    """
   un object python utiliser pour stocker les informations de chaque score
   """
-  def __init__(self, name : str, time : str) -> None:
-    self.playerName = name
-    self.score = time
+
+    def __init__(self, name: str, time: str) -> None:
+        self.playerName = name
+        self.score = time
 
 
 class LeaderBoard:
-  '''
+    '''
   une classe qui est utiliser pour stocker tous les informations concernant les score du demineur
   '''
-  def __init__(self,DBscores : dict) -> None:
-    self.scores = []
-    print("DB output is " , DBscores)
-    for item in DBscores:
-      self.scores.append([item, DBscores[item]])
- 
 
+    def __init__(self, DBscores: dict) -> None:
+        self.scores = []
+        print("DB output is ", DBscores)
+        for item in DBscores:
+            self.scores.append([item, DBscores[item]])
 
-  def get_scores(self) -> list:
-    """
+    def get_scores(self) -> list:
+        """
     returns the scores that were stored in the database in the form of a list of dictionary objects
     """
-    return self.scores
-
-
-  
+        return self.scores
 
 
 app = Flask(__name__)
 
-@app.route('/' , methods = ['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-  if request.method == 'POST':
-    print("AHA YOU POST REQUEST SCUM, PREPARE TO DIE")
-    insert_into_db(connection, request.form)
-    print(retrieve_all_from_db(connection))
-    return redirect(request.url)
-  else:
-    print("leaderboard has been asked")
-    leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
-    print("the scores are {}".format(leaderboard.get_scores()))
-    return render_template("index.html", scores = leaderboard.get_scores())
+    if request.method == 'POST':
+        print("AHA YOU POST REQUEST SCUM, PREPARE TO DIE")
+        insert_into_db(connection, request.form)
+        print(retrieve_all_from_db(connection))
+        return redirect(request.url)
+    else:
+        print("leaderboard has been asked")
+        leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
+        print("the scores are {}".format(leaderboard.get_scores()))
+        return render_template("index.html", scores=leaderboard.get_scores())
 
-@app.route("/index.html", methods = ['POST', 'GET'])
+
+@app.route("/index.html", methods=['POST', 'GET'])
 def index2():
-  if request.method == 'POST':
-    print("The request was a post and thus was the reply with the form")
-    insert_into_db(connection , request.form)
-    return redirect(request.url)
-  else:
+    if request.method == 'POST':
+        print("The request was a post and thus was the reply with the form")
+        insert_into_db(connection, request.form)
+        return redirect(request.url)
+    else:
+        print("leaderboard has been asked")
+        leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
+        print("the scores are {}".format(leaderboard.get_scores()))
+        return render_template("index.html", scores=leaderboard.get_scores())
+
+
+@app.route("/leaderboard.html", methods=['GET'])
+def leaderboard() -> None:
     print("leaderboard has been asked")
     leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
     print("the scores are {}".format(leaderboard.get_scores()))
-    return render_template("index.html", scores = leaderboard.get_scores())
-
-@app.route("/leaderboard.html", methods = ['GET'])
-def leaderboard()-> None:
-  print("leaderboard has been asked")
-  leaderboard = LeaderBoard(retrieve_all_from_db(create_connection("vault_13.db")))
-  print("the scores are {}".format(leaderboard.get_scores()))
-  return render_template("index.html", scores = leaderboard.get_scores())
+    return render_template("index.html", scores=leaderboard.get_scores())
 
 
-#this is going to be the section about sql manipulation and how to put it into a website
-#todo make the input form for ,the html file, 
-#make the back end that will recieve the variables
-#make the sql database for storing the data
-#make html file show what is in the database
+# this is going to be the section about sql manipulation and how to put it into a website
+# todo make the input form for ,the html file,
+# make the back end that will recieve the variables
+# make the sql database for storing the data
+# make html file show what is in the database
 
-#enfait au niveau de cree le leaderboard, on peut pas trigger du JS avec du python comme le JS 
-#est client side et pas server side, et python c'est server side.
+# enfait au niveau de cree le leaderboard, on peut pas trigger du JS avec du python comme le JS
+# est client side et pas server side, et python c'est server side.
 
-#donc il faut trouver comment le mettre dans le render template.
+# donc il faut trouver comment le mettre dans le render template.
 
 
 def makeLeaderBoard(data):
-  """ this is to genereate the html code for the leaderboard """
-  #leaderBoard is goign to be 2 columns with name and score
-  print(data)
+    """ this is to genereate the html code for the leaderboard """
+    # leaderBoard is goign to be 2 columns with name and score
+    print(data)
 
-if  __name__ == "__main__":
-  #base stuff
-  base_dir = os.path.dirname(os.path.abspath(__file__))
-  db_path = os.path.join(base_dir, "vault_13.db")
-  connection = create_connection(db_path)
-  #base stuff end----
-  leaderBoard = makeLeaderBoard(retrieve_all_from_db(connection))
-  
-  app.run(debug=True)
-  
-  #need to filter out all the nonesense for now
-  
+
+if __name__ == "__main__":
+    # base stuff
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(base_dir, "vault_13.db")
+    connection = create_connection(db_path)
+    # base stuff end----
+    leaderBoard = makeLeaderBoard(retrieve_all_from_db(connection))
+
+    app.run(debug=True)
+
+    # need to filter out all the nonesense for now
